921dbb7337a7d2cbbf5085503de93926a63fe9d4
Nanda <nanda.a.dimas.k@gmail.com>
2025-05-09 08:29:21 +0700

    First commit

---
+++ application/views/about.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php $this->load->view('_partials/header.php') ?>
+    </head>
+
+    <body>
+    <?php $this->load->view('_partials/navbar.php') ?>
+
+    <h1>About Us</h1>
+
+    <?php $this->load->view('_partials/footer.php') ?>
+    </body>
+</html>
